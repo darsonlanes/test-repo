87a0e3f81fad4dc2b455da6ebc50872ded1288f3
prov <provinite@gmail.com>
2015-11-23 23:58:58 -0600

    Update GitTest.java

--- src/com/larson/test/GitTest.java
+++ src/com/larson/test/GitTest.java
@@ -9,12 +9,14 @@ package com.larson.test;
  */
 public class GitTest {
     
+    //Says Hullo!
     public static void main(String[] args) {
     
         System.out.print("Hello World!");
         
     }
     
+    //Says Goodbye!
     public void goodbyeWorld() {
         
         System.out.print("Goodbye World!");
